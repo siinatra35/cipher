a6ec4df84cb21b7cd04207e2962c1f7b305bdcec
david <dacuna305@gmail.com>
2021-01-05 21:20:41 -0600

    Update path_three.py

--- path_three.py
+++ path_three.py
@@ -1,71 +1,190 @@
-
 import math
-E = ""
-D = ""
+import sys
+from alive_progress import alive_bar
+
+# Current path position:
+g_posR = 0
+g_posC = 0
+
+# Current path direction:
+g_dirR = 0
+g_dirC = 0
+
+# Current path borders:
+g_borderL = 0
+g_borderT = 0
+g_borderR = 0
+g_borderB = 0
+
+grid = []
 
 
-def storage(num_rows):
-    # arr = []
-    # pos = 0
-    # while pos < num_rows:
-    #     arr[pos] = []
-    #     pos += 1
-    arr = [0 for i in range(num_rows)]
+def fillTableForEncrypt(letters, totalRows, totalCols):
 
-    return arr
-
-# def fill_Table_Encrypt(letters, ROWS, COLS):
-#     pos = 0
-#     amount = storage(ROWS)
-#     pathType = "clockwise"
-#     # pathParams(pathType, ROWS, COLs)
-#     r = 0 
-#     c = 0
-#     for i in range(ROWS):
-#         for j in range(COLS):
-#             character = "X"
-#             if pos < len(letters):
-#                 character = letters[pos]
-#                 amount[i].append(character)
-
-#     return amount
+    for i in range(math.ceil(len(letters) / totalCols)):
+        rows = []
+        for j in range(totalCols):
+            if i * totalCols + j < len(letters):
+                rows.append(letters[i * totalCols + j])
+            else:
+                rows.append('-')
+        grid.append(rows)
+    return grid
 
 
-def pathParams(path, rowNumber, colNumber):
+def fillTableForDecrypt(letters, totalRows, totalCols, pathtype):
+    """creates decryption matrix"""
+    global g_posR, g_posC
+    newGrid = []
+
+    for i in range(math.ceil(len(letters) / totalCols)):
+        rows = []
+        for j in range(totalCols):
+            if i * totalCols + j < len(letters):
+                rows.append(letters[i * totalCols + j])
+            else:
+                rows.append('-')
+        newGrid.append(rows)
+
+    initPathParameters(pathtype, totalRows, totalCols)
+    pos = 0
+    while pos < totalRows * totalCols:
+        newGrid[g_posR][g_posC] = letters[pos]
+        makeOneStep(pathtype)
+        pos += 1
+
+    return newGrid
+
+
+def readCipherText(grid, totalRows, totalCols, pathtype):
+    initPathParameters(pathtype, totalRows, totalCols)
+    global g_posR, g_posC
+    cipher_text = ""
+
+    while len(cipher_text) < totalRows * totalCols:
+        cipher_text += grid[g_posR][g_posC]
+        makeOneStep(pathtype)
+
+    return cipher_text
+
+
+def readPlainText(matrix, totalRows, totalCols):
+    plain_text = ""
+    for i in range(totalRows):
+        for j in range(totalCols):
+            plain_text += str(matrix[i][j])
+
+    return plain_text
+
+
+def initPathParameters(pathtype, totalRows, totalCols):
+    global g_posR, g_posC, g_borderL, g_borderT, g_borderR, g_borderB, g_dirR, g_dirC
     g_posR = 0
-    g_posC = colNumber - 1
+    g_posC = totalCols - 1
+
     g_borderL = 0
     g_borderT = 0
-    g_borderR = colNumber - 1
-    g_borderB = rowNumber - 1
+    g_borderR = totalCols - 1
+    g_borderB = totalRows - 1
 
-    if path == "clockwise":
+    if pathtype == "clockwise":
         g_dirR = 1
         g_dirC = 0
-    else:
+    elif pathtype == "anticlockwise":
         g_dirR = 0
         g_dirC = -1
 
 
+def makeOneStep(pathtype):
+    global g_posR, g_posC, g_borderL, g_borderT, g_borderR, g_borderB, g_dirR, g_dirC
+
+    if g_posR + g_dirR >= g_borderT and g_posR + g_dirR <= g_borderB:
+        g_posR += g_dirR
+    else:
+        if g_dirR == 1:
+            if pathtype == "clockwise":
+                g_dirR = 0
+                g_dirC = -1
+                g_borderR -= 1
+            elif pathtype == "anticlockwise":
+                g_dirR = 0
+                g_dirC = 1
+                g_borderL += 1
+        else:
+            if pathtype == "clockwise":
+                g_dirR = 0
+                g_dirC = 1
+                g_borderL += 1
+            elif pathtype == "anticlockwise":
+                g_dirR = 0
+                g_dirC = -1
+                g_borderR -= 1
+
+    if g_posC + g_dirC >= g_borderL and g_posC + g_dirC <= g_borderR:
+        g_posC += g_dirC
+    else:
+        if g_dirC == 1:
+            if pathtype == "clockwise":
+                g_dirR = 1
+                g_dirC = 0
+                g_borderT += 1
+            elif pathtype == "anticlockwise":
+                g_dirR = -1
+                g_dirC = 0
+                g_borderB -= 1
+        else:
+            if pathtype == "clockwise":
+                g_dirR = -1
+                g_dirC = 0
+                g_borderB -= 1
+            elif pathtype == "anticlockwise":
+                g_dirR = 1
+                g_dirC = 0
+                g_borderT += 1
+
+        g_posR += g_dirR
+
+
 def main():
-    E = "DAVIDMARTNACUNA"
-    route_size = 3
+    ans = True
+    while ans:
+        option = input(
+            """Please select the desired route path.\n[1]. clockwise\n[2]. anticlockwise\n[3]. add later\n[4]. add later\n\n>>> """)
+        if option == "1":
+            pathtype = "clockwise"
+            route_size = int(input("Please enter a route size: "))
+            plain_text = input(
+                "Please enter the desired text for encryption/decryption: ")
+            ans = False
+        elif option == "2":
+            pathtype = "anticlockwise"
+            route_size = int(input("Please enter a route size:"))
+            plain_text = input(
+                "Please enter the desired text for encryption/decryption: ")
+            ans = False
+
     totalCols = route_size
-    totalRows = len(E) / totalCols
-    print(len(E))
-    
-    if totalRows != math.ceil(totalRows):
+    totalRows = len(plain_text) / totalCols
+
+    if totalRows != math.floor(totalRows):
         totalRows = math.floor(totalRows) + 1
-    #s = storage(totalRows)
-    #print(s)
+    elif type(totalRows) is float:
+        totalRows = len(plain_text) // totalCols
 
-  #  fill_Table_Encrypt(E , totalRows, totalCols)
+    grid = fillTableForEncrypt(plain_text, totalRows, totalCols)
+    encryptedText = readCipherText(grid, totalRows, totalCols, pathtype)
+    print("Encrypted Text :", encryptedText)
 
+    new_grid = []
+    if totalRows != math.floor(totalRows):
+        print("The length does not match the table dimensions.")
+        sys.exit(0)
+    else:
+        new_grid = fillTableForDecrypt(
+            encryptedText, totalRows, totalCols, pathtype)
+        decryptedText = readPlainText(new_grid, totalRows, totalCols)
+        print("Decrypted Text :", decryptedText)
 
 
 if __name__ == "__main__":
     main()
-    
-
-
-
